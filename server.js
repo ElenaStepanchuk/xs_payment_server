683c896349c20488d1572b1a2b6729d164297d29
Elena <83465804+ElenaStepanchuk@users.noreply.github.com>
2024-09-19 17:21:14 +0300

    add  stripe

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const cors = require("cors");
-require("dotenv").config();
 const express = require("express");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
 const app = express();
-
 app.use(cors());
+const stripe = require("stripe")(
+  "sk_test_51PzcOC03tMrlIcEhwUAWIpO0tDV8E6w5VOiieoYNfWqyKuzO9FhiRBJ9rW8Q6lVfcUlVZlOw6h2AucmS1Ko2dAMx0098bUtcmz"
+);
+
+app.use(express.static("public"));
 app.use(express.json());
 
+const calculateOrderAmount = (items) => {
+  let total = 0;
+  items.forEach((item) => {
+    total += item.amount;
+  });
+  return total;
+};
+
 app.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
+  const { items } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "chf",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
 
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "chf",
-      payment_method_types: ["twint"],
-    });
-
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error("Error creating Payment Intent:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+  });
 });
 
-app.listen(4000, () => console.log("Server running on port 4000"));
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
